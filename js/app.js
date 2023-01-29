0c1c6542955579ba0c8ffb03e69c071f52e4f247
Nyi Htut Lwin <lwinnyihtut0@gmail.com>
2023-01-29 13:35:20 +0630

    clean code in app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,43 @@
-// Items' Information
 // btn for widget 
-
 const informationItem = document.querySelectorAll('.widget-glass');
 //###
+
 // selected for glass item information div
 const IteminfoOne =document.querySelector('.paragrah-1');
 const IteminfoTwo =document.querySelector('.paragrah-2');
 const IteminfoThr =document.querySelector('.paragrah-3');
 //###
+
+//slideshow categories
+const projectAlu =document.querySelector('.swiper-1');
+const projectBat =document.querySelector('.swiper-2');
+const projectGpa =document.querySelector('.swiper-3');
+const projectGsc =document.querySelector('.swiper-4');
+const projectHar =document.querySelector('.swiper-5');
+//###
+
+// explore our project section 
+const projectBtn =document.querySelectorAll('.item');
+// ###
+
+// client testimonials slider btn
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+const slides=document.querySelectorAll('.slide');
+//###
+
+// In client testimonial section, want to blur the other widget when it'd been clicked to current widget
+let infoOne =document.querySelector('.info-1').style;
+let infoTwo =document.querySelector('.info-2').style;
+let infoThree =document.querySelector('.info-3').style;
+//###
+
+// for footer section, to get current full year
+const year = document.querySelector('.year');
+//###
+
+
+// Items' Information
  informationItem.forEach(p=>{
      p.addEventListener('click',o=>{
         const eashItem =o.currentTarget.classList;
@@ -27,16 +57,8 @@ const IteminfoThr =document.querySelector('.paragrah-3');
      })
  })
 // #######
-// Explore Our Project
-const projectBtn =document.querySelectorAll('.item');
 
-//slideshow categories
-const projectAlu =document.querySelector('.swiper-1');
-const projectBat =document.querySelector('.swiper-2');
-const projectGpa =document.querySelector('.swiper-3');
-const projectGsc =document.querySelector('.swiper-4');
-const projectHar =document.querySelector('.swiper-5');
-//######
+// Explore Our Project
  projectBtn.forEach((c)=>{
      c.addEventListener('click',(k)=>{
         const target =k.currentTarget.classList;
@@ -77,13 +99,10 @@ const projectHar =document.querySelector('.swiper-5');
          }
      })
  })
-
 // #######
 
-//slide
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.prev');
-const slides=document.querySelectorAll('.slide');
+// client testimonial silder
+
 slides.forEach((slide,i)=>{
     slide.style.left=`${i*100}%`
 });
@@ -106,39 +125,16 @@ function slider(){
     if(counter<0){
         counter=slides.forEach.length-1
     }
-    // for the nextBtn
-    // if(counter<slides.length-1){
-    //     nextBtn.style.display='block'
-    //     nextBtn.style.opacity='100%'
-    // }else{
-    //     nextBtn.style.opacity='0%'
-    //     nextBtn.style.display='none'
-    // }
-    // ######
-    // for the prevBtn
-    // if(counter>0){
-    //     prevBtn.style.display='block'
-    //     prevBtn.style.opacity='100%'
-    // }else{
-    //     prevBtn.style.display='none'
-    //     prevBtn.style.opacity='0%'
-    // }
-    // #####
+
+    
     slides.forEach((slide)=>{
         slide.style.transform=`translateX(-${counter*100}%)`
     })
 }
 // #######
 
-//  dynamic to set the date
-const year = document.querySelector('.year');
-year.innerHTML=new Date().getFullYear();
 
-// #######
-let infoOne =document.querySelector('.info-1').style;
-let infoTwo =document.querySelector('.info-2').style;
-let infoThree =document.querySelector('.info-3').style;
-
+// client testimonials section. Blur the widget
 function toggle(value){
     switch (value) {
         case 1:
@@ -160,3 +156,8 @@ function toggle(value){
           break;
       }
 }
+// ###
+
+//set the current year in footer section
+year.innerHTML=new Date().getFullYear();
+// #######
